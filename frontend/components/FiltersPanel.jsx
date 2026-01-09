9b89953009bb7fe617a6bd6dc5aebf65e2fc104f
Krishna Prajapati <krishnaprajapat8814@outlook.com>
2026-01-09 22:50:04 +0530

    Updated Filter Pane

--- frontend/components/FiltersPanel.jsx
+++ frontend/components/FiltersPanel.jsx
@@ -82,10 +82,10 @@ function FiltersPanel({ onClose, onApply }) {
                          text-slate-200"
             >
               <option value="">All Years</option>
-              <option value="2016">2016</option>
-              <option value="2017">2017</option>
-              <option value="2018">2018</option>
-              <option value="2019">2019</option>
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
             </select>
           </div>
 
