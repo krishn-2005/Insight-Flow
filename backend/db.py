de286cecb31cef46539eea0b1c63a144ce4106c7
Krishna Prajapati <krishnaprajapat8814@outlook.com>
2026-01-13 15:38:37 +0530

    backend changes and added SS folder

--- backend/db.py
+++ backend/db.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
